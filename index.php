06c308fc0d6a6dabc6cf710ba4de3c2aeb98ea69
Gabriel3465 <gabriel2_23@alu.uabcs.mx>
2025-12-01 19:24:56 -0700

    update: dejar de subir archivos de logs

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-ini_set('log_errors', 1);
-ini_set('error_log', __DIR__ . '/logs/php_errors.log');
+ini_set('', 1);
 
 require_once __DIR__ . '/vendor/autoload.php';
 session_start();
@@ -16,11 +15,5 @@ require_once __DIR__ . '/routes/web.php';
 try {
     $router->dispatch();
 } catch (Exception $e) {
-    error_log("Router Error: " . $e->getMessage() . "\n" . $e->getTraceAsString());
-    
-    echo "<pre>";
-    echo "Router Error: " . $e->getMessage() . "\n";
-    echo "Stack trace:\n" . $e->getTraceAsString();
-    echo "</pre>";
-    die();
+    die("Router Error: " . $e->getMessage());
 }
